b36fb3e444681dc5e832697ba10974693c3fdf85
Daria Bernice <daria.bernice.calitis@mths.ca>
2021-09-20 17:45:10 +0000

    updated guessGame.cpp

--- guessGame.cpp
+++ guessGame.cpp
@@ -10,25 +10,26 @@
 
 int main() {
     // This function checks if your guess is correct
+    int answer;
+    int guessNumber;
 
     std::random_device rseed;
     std::mt19937 rgen(rseed());  // mersenne_twister
     std::uniform_int_distribution<int> idist(0, 9);  // [0,9]
 
-    const int ANSWER = idist(rgen);
-    int guessNumber;
+    answer = idist(rgen);
 
     // input
     std::cout << "Enter a number as your guess (0-9): ";
     std::cin >> guessNumber;
 
     // process & output
-    if (guessNumber == ANSWER) {
+    if (guessNumber == answer) {
         std::cout << "You guessed correctly!" << std::endl;
     } else {
         std::cout << "You guessed incorrectly." << std::endl;
     }
 
-    std::cout << "The correct answer is " << ANSWER << "." << std::endl;
+    std::cout << "The correct answer is " << answer << "." << std::endl;
     std::cout << "\nDone." << std::endl;
 }
